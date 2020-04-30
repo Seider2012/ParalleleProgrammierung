4fc7e5bedbb0cf61191e1089f117ed06cd52e100
Bert Ödemis <bert.oedemis@student.uibk.ac.at>
2020-04-30 19:24:51 +0200

    EX1

--- week6/EX1.c
+++ week6/EX1.c
@@ -49,34 +49,34 @@ int calculateSeqSize(const char *arg) {
     return folder_size;
 }
 
-void measureMethod(const char *arg, struct dirent *pDirent, size_t *size) {
+int measureMethod(const char *arg, struct dirent *pDirent) {
 
     int cur_size;
     struct stat buffer;
     char *fullpath = malloc(strlen(arg) + strlen(pDirent->d_name) + 2);
     if (fullpath == NULL) { fprintf(stderr, "Error creating path: %s\n", strerror(errno)); }
     sprintf(fullpath, "%s\\%s", arg, pDirent->d_name);
+
+    if (pDirent->d_type == DT_DIR) {
+        cur_size = calculateParSize(fullpath);
+    }else{
     int stat = lstat(fullpath, &buffer);
     cur_size = buffer.st_size;
     if (stat == -1) {
         fprintf(stderr, "Error opening file: %s\n", strerror(errno));
-    } else if (cur_size == 0) {
-        cur_size = calculateParSize(fullpath);
     }
 
 
-    printf("[%s] size=%d\n", pDirent->d_name, cur_size);
+   // printf("[%s] size=%d\n", pDirent->d_name, cur_size);
+    }
 
     free(fullpath);
-#pragma omp atomic
-    *size += cur_size;
+return cur_size;
 
 }
 
 
 int calculateParSize(const char *arg) {
-    struct dirent *pDirent;
-
     DIR *pDir;
     size_t folder_size = 0;
     pDir = opendir(arg);
@@ -84,44 +84,44 @@ int calculateParSize(const char *arg) {
         fprintf(stderr, "Cannot open directory '%s'\n", arg);
         return -1;
     }
-
-
 #pragma omp parallel reduction(+:folder_size)
-#pragma omp single
+#pragma omp master
     {
-
-        struct dirent **dirents = (struct dirent **) malloc(sizeof(struct dirent) * 2);
+        struct dirent *dirents[2];
         do {
             dirents[0] = readdir(pDir);
             if (dirents[0] != NULL) {
                 while ((dirents[0]->d_name[0] == '.' &&
                         (dirents[0]->d_name[1] == '.' ||
-                         dirents[0]->d_name[1] == '\0')))
-                {
+                         dirents[0]->d_name[1] == '\0'))) {
                     dirents[0] = readdir(pDir);
                     if (dirents[0] == NULL)
                         break;
                 }
             }
+            if (dirents[0] != NULL) {
+                struct dirent *temp = (struct dirent *) malloc(sizeof(struct dirent));
+                memcpy(temp, dirents[0], sizeof(struct dirent));
+                dirents[0] = temp;
+            }
             dirents[1] = readdir(pDir);
-
-
 #pragma omp task shared(arg, dirents, folder_size)
             {
-                if(*dirents!=NULL){
-                    measureMethod(arg, *dirents, &folder_size);
+                if (dirents[0] != NULL) {
+                    folder_size+= measureMethod(arg, dirents[0]);
+                } else {
+                    free(dirents[0]);
                 }
             }
 #pragma omp task shared(arg, dirents, folder_size)
             {
-                if(*(dirents+1)!=NULL){
-                    measureMethod(arg,*(dirents+1), &folder_size);
+                if (dirents[1] != NULL) {
+                    folder_size+= measureMethod(arg, dirents[1]);
                 }
             }
 
 #pragma omp taskwait
-        } while (dirents[1] != NULL);
-        free(dirents);
+        }while (dirents[1] != NULL);
     }
 // close dir
     closedir(pDir);
@@ -135,7 +135,9 @@ int main(int argc, char *argv[]) {
     }
     double start_time;
     double end_time;
-    int size;
+    unsigned long long size;
+
+/*
     start_time = omp_get_wtime();
     size = calculateSeqSize(argv[1]);
     end_time = omp_get_wtime();
@@ -148,6 +150,7 @@ int main(int argc, char *argv[]) {
 
     printf("Sequenziel size=%d Bytes time: %2.4f seconds\n", size,
            end_time - start_time);
+*/
 
     start_time = omp_get_wtime();
     size = calculateParSize(argv[1]);
@@ -158,7 +161,7 @@ int main(int argc, char *argv[]) {
     }
 
 
-    printf("Parallel size=%d Bytes time: %2.4f seconds\n", size,
+    printf("Parallel size=%u Bytes time: %2.4f seconds\n", size,
            end_time - start_time);
     return EXIT_SUCCESS;
 }
