12bca4305de4ee6971c054e2f2f622050cda1687
Bert Ödemis <bert.oedemis@student.uibk.ac.at>
2020-06-11 15:40:03 +0200

    week12

---
+++ week12/jobEX2_2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Executes job in the queue "std.q" unless you have special requirements.
+#$ -q std.q
+
+# Changes to the current working directory before performing any further action
+#$ -cwd
+
+# Name of your job. Unless you use the -o and -e options, output will
+# go to a unique file name.ojob_id for each job.
+#$ -N ResEx2_2
+
+# Redirect output stream to this file.
+#$ -o ResEx2_2.dat
+
+# Join the error stream to the output stream.
+#$ -j yes
+
+# Parallel environment for using OpenMP, allocates 8 cores on a single node
+#$ -pe openmp 8
+
+#$ -l h_vmem=2G
+
+
+# Set up any environment variables
+#export OMP_NUM_THREADS=8
+
+# Use gcc 8.2.0 as the default gcc
+module load gcc/8.2.0
+ gcc -std=c11 -O1 -fopenmp EX2_1.c -o EX2_1.out
+perf stat -e rC701 ./EX2_1.out
+perf stat -e rC702 ./EX2_1.out
+perf stat -e rC704 ./EX2_1.out
+perf stat -e rC708 ./EX2_1.out
+perf stat -e rC710 ./EX2_1.out
+perf stat -e rC7F1 ./EX2_1.out
+ gcc -std=c11 -O1 -fopenmp EX2_2.c -o EX2_2.out
+perf stat -e rC701 ./EX2_2.out
+perf stat -e rC702 ./EX2_2.out
+perf stat -e rC704 ./EX2_2.out
+perf stat -e rC708 ./EX2_2.out
+perf stat -e rC710 ./EX2_2.out
+perf stat -e rC7F1 ./EX2_2.out
+#export OMP_NUM_THREADS=1
