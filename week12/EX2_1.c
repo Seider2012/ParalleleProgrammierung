51c754269ae8dacbab4a284faf0e60416dddc0ed
Bert Ödemis <bert.oedemis@student.uibk.ac.at>
2020-06-11 15:00:24 +0200

    week12

---
+++ week12/EX2_1.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+//#define N 1000
+
+int main(int argc, void **argv) {
+    int repetitions = 1000000;
+    int N = 1000;
+    if (argc == 2) {
+        char **p;
+        N = (int) strtol(argv[1], p, 10);
+
+    }
+    double start, end;
+
+    float *a = aligned_alloc(32, sizeof(float));
+    float *b = aligned_alloc(32, sizeof(float));
+    float *c = aligned_alloc(32, sizeof(float));
+    float c1, c2, c3;
+    c1 = 0;
+    c2 = 1;
+    c3 = 2;
+    for (int i = 0; i < N; ++i) {
+        a[i] = c1;
+        b[i] = c2;
+        c[i] = c3;
+    }
+    start = omp_get_wtime();
+#pragma omp simd aligned(a, b, c:32)
+    for (int run = 0; run < repetitions; ++run) {
+        for (int i = 0; i < N; ++i) {
+            a[i] += b[i] * c[i];
+        }
+    }
+    end = omp_get_wtime();
+    int correctness = 1;
+    for (int i = 0; i < N; ++i) {
+        //printf("%f = %f * %f\n",a[i],b[i],c[i]);
+        if (a[i] == N * c2 * c3) {
+            correctness = 0;
+        }
+    }
+
+    printf("%s, N= %d, time = %f s\n", correctness ? "SUCCESS" : "FAILURE", N, end - start);
+
+
+}
--- week12/jobEX2.sh
+++ week12/jobEX2.sh
@@ -27,11 +27,12 @@
 
 # Use gcc 8.2.0 as the default gcc
 module load gcc/8.2.0
- gcc -std=c99 -O1 -fopenmp EX2.c -o EX2.out
-./EX2.out 0 1000
-./EX2.out 0 500
-./EX2.out 0 100
-./EX2.out 1 1000
-./EX2.out 1 500
-./EX2.out 1 100
+ gcc -std=c99 -O1 -fopenmp EX2_1.c -o EX2_1.out
+./EX2_1.out 1000
+./EX2_1.out 500
+./EX2_1.out 100
+ gcc -std=c99 -O1 -fopenmp EX2_2.c -o EX2_2.out
+./EX2_2.out 1 1000
+./EX2_2.out 1 500
+./EX2_2.out 1 100
 #export OMP_NUM_THREADS=1
